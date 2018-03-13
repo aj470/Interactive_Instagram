7681c8e2337b1afd28a5ab9d144a03f77981f502
Sanjit Singh <sanjitsingh@Sanjits-MacBook-Air.local>
2018-03-12 20:25:55 -0400

    working on VR

---
+++ Interactive_Instagram_VR/Assets/Scripts/CameraControlZeroG.cs
@@ -0,0 +1,85 @@
+﻿/*
+ * Copyright 2014 Jason Graves (GodLikeMouse/Collaboradev)
+ * http://www.collaboradev.com
+ *
+ * This file is part of Unity - Topology.
+ *
+ * Unity - Topology is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public 
+ * License as published by the Free Software Foundation, either 
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * Unity - Topology is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with Unity - Topology. If not, see http://www.gnu.org/licenses/.
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CameraControlZeroG : MonoBehaviour {
+
+	public float speed = 12f;
+    public Text movementSpeed;
+	private CharacterController myController;
+ 
+
+    private Vector3 move = new Vector3();
+
+	 
+
+	 
+
+    void Update () {
+		
+		//move.x = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
+		//move.z = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+
+		//move.x = Input.GetAxis ("Mouse X") * speed * Time.deltaTime;
+		//move.z = Input.GetAxis ("Mouse Y") * speed * Time.deltaTime;
+
+		 
+
+
+		move.y = 0;
+		if (Input.GetKey ("w")) {
+			move.y = speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("s")) {
+			move.y = -speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("a")) {
+			move.x = -speed * Time.deltaTime;
+		}
+
+		if (Input.GetKey ("d")) {
+			move.x = speed * Time.deltaTime;
+		}
+
+	
+
+		//Vector3 moveforward = Input.GetAxis ("Fire4") * Vector3.forward * speed * Time.deltaTime;
+		//Vector3 movebackward = Input.GetAxis ("Fire5") * Vector3.back * speed * Time.deltaTime;
+		//Vector3 Movement = transform.TransformDirection (moveforward + movebackward);
+//		Debug.Log ("Movement " + Movement);
+ 
+	
+
+		//adjust speed with mouse wheel
+		speed += Input.GetAxis("Mouse ScrollWheel") * 15;
+		if (speed < 5)
+			speed = 5;
+
+		movementSpeed.text = "Flyspeed: " + speed;
+
+		move = transform.TransformDirection(move);
+		transform.position += move;
+	}
+}
---
+++ Interactive_Instagram_VR/Assets/Scripts/MouseLooker.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLooker : MonoBehaviour {
+
+	//public variables
+	public float Xsen = 2f;
+	public float Ysen = 2f;
+	public bool ClampVerticalRotation = true;
+	public float MinX = -90f;
+	public float MaxX = 90f;
+	public bool smooth;
+	public float Smoothtime = 5f;
+
+
+	//private variables
+	private Quaternion CharacterTargetRot;
+	private Quaternion CameraTargetRot;
+	private Transform Character; 
+	private Transform CameraTransform;
+
+	// Use this for initialization
+	void Start () {
+		LockCursor (false);
+
+		//get reference to our character transform(specifically for the script we attach to the specific layer of our game)
+		//Character = gameObject.transform;
+
+		//get a reference to camera's transform
+		CameraTransform = Camera.main.transform;
+
+		//get location of our camera and character
+		//CharacterTargetRot = Character.localRotation;
+		CameraTargetRot = CameraTransform.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LookRotation ();
+
+		if (Input.GetButtonDown ("Cancel")) {
+			LockCursor (false);
+		}
+		if (Input.GetButtonDown ("Fire1")) {
+			LockCursor (true);
+		}
+	}
+
+	private void LockCursor(bool isLocked) {
+		if (isLocked) 
+		{
+			// make the mouse pointer invisible
+			Cursor.visible = false;
+
+			// lock the mouse pointer within the game area
+			Cursor.lockState = CursorLockMode.Locked;
+		} else {
+			// make the mouse pointer visible
+			Cursor.visible = true;
+
+			// unlock the mouse pointer so player can click on other windows
+			Cursor.lockState = CursorLockMode.None;
+		}
+	}
+
+	public void LookRotation() {
+		//get x and y rotation behind input manager
+		float yRot = Input.GetAxis ("Mouse X") * Xsen;
+
+		float xRot = Input.GetAxis ("Mouse Y") * Ysen;
+
+		//CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
+
+		CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
+
+		if(ClampVerticalRotation)
+			CameraTargetRot = ClampRotationAroundXAxis (CameraTargetRot);
+
+		// update the character and camera based on calculations
+		if(smooth) 
+			// if smooth, then slerp over time
+		{
+			//Character.localRotation = Quaternion.Slerp (Character.localRotation, CharacterTargetRot,
+			//	Smoothtime * Time.deltaTime);
+			CameraTransform.localRotation = Quaternion.Slerp (CameraTransform.localRotation, CameraTargetRot,
+				Smoothtime * Time.deltaTime);
+		}
+		else 
+			// not smooth, so just jump
+		{
+			//Character.localRotation = CharacterTargetRot;
+			CameraTransform.localRotation = CameraTargetRot;
+		}
+	}
+
+	//Some math
+	Quaternion ClampRotationAroundXAxis (Quaternion q) 
+	{
+		q.x /= q.w;
+		q.y /= q.w;
+		q.z /= q.w;
+		q.w = 1.0f;
+
+		float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+		angleX = Mathf.Clamp (angleX, MinX, MaxX);
+		q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
+		return q;
+	}
+}
